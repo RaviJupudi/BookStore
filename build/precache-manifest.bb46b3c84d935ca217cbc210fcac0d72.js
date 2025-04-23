a6c3278b5f777e8fb717715207b78ad7c1e625f8
RaviJupudi <ravi.jupudii@gmail.com>
2025-04-23 07:01:44 -0700

    build

---
+++ build/precache-manifest.bb46b3c84d935ca217cbc210fcac0d72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65de2fe48aa59d434e815eaac2de290f",
+    "url": "/RaviJupudi.github.io/BookStore/index.html"
+  },
+  {
+    "revision": "f241c533419147ad660a",
+    "url": "/RaviJupudi.github.io/BookStore/static/css/main.ce69a0b8.chunk.css"
+  },
+  {
+    "revision": "8cee68188cb1477f1f1f",
+    "url": "/RaviJupudi.github.io/BookStore/static/js/2.ec32403c.chunk.js"
+  },
+  {
+    "revision": "f241c533419147ad660a",
+    "url": "/RaviJupudi.github.io/BookStore/static/js/main.9694a726.chunk.js"
+  },
+  {
+    "revision": "484ea7f6b61e760af995",
+    "url": "/RaviJupudi.github.io/BookStore/static/js/runtime~main.0548b7d5.js"
+  }
+]);
